a2bde8d3704c14409a5d6d6790a6f9cb2c4ea085
Guilherme <guilhermemoraes1826@gmail.com>
2023-07-12 16:00:21 -0300

    Port server configuration

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ app.use((err, req, res, next) => {
     res.status(status).render('error', { data })
 })
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
 });
